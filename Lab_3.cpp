4407fbb50d7951dd468be4e1ed750c653a5e5a20
Ilyanast <miningilya4@gmail.com>
2020-10-29 12:34:08 +0300

    add SFML

--- Lab_3.cpp
+++ Lab_3.cpp
@@ -1,16 +1,26 @@
 ﻿#include <iostream>
 #include  "Train.h"
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	Train* train = new Train(1001, 90, 20000);
-	train->add_passenger_van(1002, 2000, 20, 100);
-	train->add_freight_van(1003, 1000, 2000, 20000);
-	train->add_passenger_van(1005, 2000, 40, 100);
-	train->add_passenger_van(1006, 2000, 40, 120);
-	train->add_freight_van(1007, 2000, 2000, 10000);
-	train->add_passenger_van(1008, 2000, 45, 120);
-	train->add_passenger_van(1009, 2000, 45, 120);
-	train->add_locomative(1004, 100, 60000);
-	getchar();
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
 }
